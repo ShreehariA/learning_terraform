1ab0a2301aa7119641e637672fdc6c3d38986ade
Shreehari Anbazhagan <122812415+ShreehariA@users.noreply.github.com>
2023-03-27 17:02:10 +0530

    name change in as

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.9.0"
   
-  name     = "${var.environment.name}-blog"
+  name     = "${var.environment.name}"
   min_size = var.asg_min_size
   max_size = var.asg_max_size
 
